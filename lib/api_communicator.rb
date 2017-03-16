10e28ccfcf949fb37bcbbc8cb3335e4ecd591833
David Mieloch <davidmieloch@gmail.com>
2017-03-16 15:03:36 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,60 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+def titlecase(string)
+  string.split(" ").map(&:capitalize).join(" ")
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def get_character_weight(character)
+  character_hash = fetch_characters
+
+  character_hash["results"].each do |characters|
+    characters.each do |key, value|
+      if character == characters["name"].downcase
+        character_titlecase = titlecase(character)
+        puts "#{character_titlecase} is a SHOCKING #{characters["mass"]} kg!!"
+        return
+      end
+    end
+  end
+end
+
+def fetch_characters
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+end
+
+def get_character_movies_from_api(character_name)
+  character_hash = fetch_characters
+
+  character_data = character_hash["results"].each do |character_hash|
+    character_hash["name"].downcase == character_name
+  end
+
+  film_urls = character_data.first["films"]
+
+  film_urls.collect do |film_url|
+    film_response = RestClient.get(film_url)
+    JSON.parse(film_response)
+  end
+end
+
+def parse_character_movies(films)
+  films.each.with_index(1) do |film, i|
+    puts "#{i}. #{film['title']}"
+  end
 end
 
 def show_character_movies(character)
+  puts "Building film list"
+  sleep(1)
+  puts "."
+  sleep(1)
+  puts ".."
+  sleep(1)
+  puts "..."
+  sleep(1)
+  puts "...."
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
